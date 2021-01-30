72b4e2240a71519a79e6b378a132b9855e035673
Tommyzgx <1146878597zgx@gmail.com>
2021-01-30 05:32:52 -0500

    new

--- server.js
+++ server.js
@@ -12,12 +12,7 @@ const server = express()
 
 const io = socketIO(server);
 
-io.on('connection', (socket) => {
-  console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
-});
-
 io.on('send_message', (data) => {
-  io.emit('time', data);
+  io.emit('time', "hi broo");
 });
 
